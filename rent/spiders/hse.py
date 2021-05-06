aea60f3092c8520017069d715ebe0f127cfbb76d
Fung Yeung <yeung.kei.fung@gmail.com>
2021-05-06 20:55:22 +0800

    add pagination logic, now possible to fetch all pages given the form_data

--- rent/spiders/hse.py
+++ rent/spiders/hse.py
@@ -36,32 +36,46 @@ class HseSpider(scrapy.Spider):
     name = 'hse'
     allowed_domains = ['28hse.com']
     start_urls = ['https://www.28hse.com/en/rent']
-    base = 'https://www.28hse.com/en/rent'
+    fetch_next_page = None
+    page = 1
+    form_data = {'form_data': 'buyRent=rent&sortBy=&plan_id=0&plan_id_more_search_open=&page=1&location=hk&district_ids=0&district_group_ids=10&cat_ids=&house_main_type_ids=5&house_other_main_type_ids=&house_other_main_type_id_fix=0&house_other_sub_main_type_ids=&price_selection_index=0&price_low=0&price_high=0&rentprice_selection_index=0&rentprice_low=0&rentprice_high=0&area_selection_index=1&area_build_sales=build&area_low=0&area_high=&noOfRoom=&estate_age_low=0&estate_age_high=0&house_search_tag_ids=&myfav=&myvisited=&property_ids=&is_return_newmenu=0&is_grid_mode=0&landlordAgency=&estate_age_index=&temp_house_search_tag_ids=&search_words_value=&search_words_thing=&search_words=&sortByBuy=&sortByRent='}
 
     def parse(self, response):
         """send form reqeust with desired criteria"""
-        form_data = {'form_data': 'buyRent=rent&sortBy=&plan_id=0&plan_id_more_search_open=&page=1&location=hk&district_ids=0&district_group_ids=10&cat_ids=&house_main_type_ids=5&house_other_main_type_ids=&house_other_main_type_id_fix=0&house_other_sub_main_type_ids=&price_selection_index=0&price_low=0&price_high=0&rentprice_selection_index=0&rentprice_low=0&rentprice_high=0&area_selection_index=1&area_build_sales=build&area_low=0&area_high=300&noOfRoom=&estate_age_low=0&estate_age_high=0&house_search_tag_ids=&myfav=&myvisited=&property_ids=&is_return_newmenu=0&is_grid_mode=0&landlordAgency=&estate_age_index=&temp_house_search_tag_ids=&search_words_value=&search_words_thing=&search_words=&sortByBuy=&sortByRent='}
         yield scrapy.FormRequest(
             url='https://www.28hse.com/en/property/dosearch',
-            formdata=form_data,
+            formdata=self.form_data,
             callback=self.parse_properties
         )
 
     def parse_properties(self, response):
-        """fetch properties id in page"""
-
+        """fetch properties id in page if any,
+        if there is no next page (regex match none), then the scraper would stop itself"""
+        self.fetch_next_page = False # in the new page assume there is no item
         out = json.loads(response.text)['data']['results']['resultContentHtml']
         for line in out.splitlines():
             if m:=re.match(r".*attr1=\'(\d*)\'.* target=\"_blank\">$", line):
-                # print(o)
+                fetch_next_page = True
                 properties_id = m.group(1)
                 yield scrapy.FormRequest(
                     url=f'https://www.28hse.com/en/rent/residential/property-{properties_id}',
                     callback=self.parse_property_info
                 )
 
+        # get next page
+        if fetch_next_page:
+            self.form_data['form_data'] = self.form_data['form_data'].replace(f'&page={self.page}&location',
+                                                                              f'&page={self.page+1}&location')
+            self.page += 1
+            yield scrapy.FormRequest(
+            url='https://www.28hse.com/en/property/dosearch',
+            formdata=self.form_data,
+            callback=self.parse_properties
+        )
+
     def parse_property_info(self, response):
         """parse property page infos"""
+        # inspect_response(response, self)
 
         output = {}
         from scrapy import Selector
