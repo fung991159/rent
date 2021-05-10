67e80685eb97f05fa64d247ad6d336ad34540449
Fung Yeung <yeung.kei.fung@gmail.com>
2021-05-10 18:50:27 +0800

    added data cleansing step

--- rent/pipelines.py
+++ rent/pipelines.py
@@ -6,8 +6,27 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import re
 
 class RentPipeline:
     def process_item(self, item, spider):
+
+        id = re.match('.*property-(\d*)', item['url']).group(1)
+        item['id'] = int(id)
+
+        rent_amt = re.match('^Lease \$(.*)$', item['monthly_rental']).group(1)
+        rent_amt = rent_amt.replace(',', '')
+        item['monthly_rental'] = int(rent_amt)
+
+        ads_type = re.match('.*\[(.*) Ads\]', item['title']).group(1)
+        item['ads_type'] = ads_type
+
+        vistor_cnt = re.match('.*#(\d*).*', item['tag']).group(1)
+        item['vistor_cnt'] = int(vistor_cnt)
+
+        for a in ['salesable_area', 'building_area']:
+            if item.get(a):
+                area = re.match('(\d.*) Sq. Feet.*', item[a]).group(1)
+                item[a] = int(area)
+
         return item
